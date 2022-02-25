6eafa5f6d0c35b7dc01d03c6b983f0e15995c087
artrayme <artrayme@protonmail.com>
2022-02-25 23:55:19 +0300

    fix(sc-controller): add sc-memory manager dependency

--- sc-json-http-module/src/main/java/org/wink/module/http/scg/controller/ScElementController.java
+++ sc-json-http-module/src/main/java/org/wink/module/http/scg/controller/ScElementController.java
@@ -1,5 +1,6 @@
 package org.wink.module.http.scg.controller;
 
+import org.ostis.scmemory.model.exception.ScMemoryException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +10,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wink.engine.analyser.autocompleter.Autocompleter;
-import org.wink.engine.model.graph.interfaces.WinkElement;
+import org.wink.engine.exceptions.CannotCreateEdgeException;
+import org.wink.engine.exceptions.CannotCreateLinkException;
+import org.wink.engine.exceptions.CannotCreateNodeException;
+import org.wink.engine.exceptions.GraphWithThisNameAlreadyUploadedException;
+import org.wink.engine.model.graph.impl.DefaultWinkGraphHeader;
+import org.wink.engine.model.graph.interfaces.WinkGraph;
+import org.wink.engine.scmemory.ScMemoryManager;
 import org.wink.module.http.scg.dto.AutocompletionDto;
 import org.wink.module.http.scg.dto.ScElementDto;
 import org.wink.module.http.scg.mapper.ScJsonMapper;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/element")
@@ -23,15 +31,48 @@ public class ScElementController {
     private final ScJsonMapper scJsonMapper;
     private final Autocompleter autocompleter;
 
+    //    ToDo Mikita
+    //    Please, inject this dependency correctly.
+    //    @artrayme just added it.
+    //    At now OstisScMemoryManager implementation should be injected.
+    //    I think, the best way to create this class is to read URI of the OSTIS from the config file.
+    //    Default URI -- ws://localhost:8090/ws_json.
+    //    You can find an example here -- OstisScMemoryManagerTest.
+    private final ScMemoryManager manager;
+
     @Autowired
-    public ScElementController(ScJsonMapper scJsonMapper, Autocompleter autocompleter) {
+    public ScElementController(ScJsonMapper scJsonMapper, Autocompleter autocompleter, ScMemoryManager manager) {
         this.scJsonMapper = scJsonMapper;
         this.autocompleter = autocompleter;
+        this.manager = manager;
     }
 
     @PostMapping
     public ResponseEntity<?> save(List<ScElementDto> scElements) {
-        List<WinkElement> winkElements = scJsonMapper.map(scElements);
+        WinkGraph graph = scJsonMapper.map(scElements, new DefaultWinkGraphHeader("NOT_SPECIFIED", "NOT_SPECIFIED"));
+
+        //        ToDo Mikita
+        //        1) Graph can be loaded only with a unique name.
+        //        For example, file path.
+        //        And this name can be passed in the request (with List of dto).
+        //
+        //        2) It is better to return error explanation,
+        //        because is not difficult for realisation but very useful for user.
+        //        Please, try to return normal response according to caught exception (in blocks bellow).
+        //
+        try {
+            manager.upload("TODO_unique_name", graph);
+        } catch (ScMemoryException e) {
+            e.printStackTrace();
+        } catch (GraphWithThisNameAlreadyUploadedException e) {
+            e.printStackTrace();
+        } catch (CannotCreateNodeException e) {
+            e.printStackTrace();
+        } catch (CannotCreateLinkException e) {
+            e.printStackTrace();
+        } catch (CannotCreateEdgeException e) {
+            e.printStackTrace();
+        }
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
