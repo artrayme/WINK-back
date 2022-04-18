034c16b0aa7fef0bb3ad1118d8054822c360147d
nikitatorop <torop.nikita1@gmail.com>
2022-04-18 14:10:41 +0300

    implement ScPatternJsonMapper

--- sc-json-http-module/src/main/java/org/wink/module/http/scg/controller/ScPatternTripletController.java
+++ sc-json-http-module/src/main/java/org/wink/module/http/scg/controller/ScPatternTripletController.java
@@ -1,5 +1,6 @@
 package org.wink.module.http.scg.controller;
 
+import org.ostis.scmemory.model.pattern.ScPattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,9 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.wink.module.http.scg.dto.AutocompletionDto;
 import org.wink.module.http.scg.dto.ScPatternTripletDto;
-import org.wink.module.http.scg.mapper.WebsocketScJsonMapper;
+import org.wink.module.http.scg.mapper.ScPatternJsonMapper;
 
 import java.util.List;
 
@@ -18,18 +18,19 @@ import java.util.List;
  * @since 0.0.1
  */
 @RestController
-@RequestMapping("/element")
+@RequestMapping("/triplet")
 public class ScPatternTripletController {
 
-    private final WebsocketScJsonMapper websocketScJsonMapper;
+    private final ScPatternJsonMapper scPatternJsonMapper;
 
     @Autowired
-    public ScPatternTripletController(WebsocketScJsonMapper websocketScJsonMapper) {
-        this.websocketScJsonMapper = websocketScJsonMapper;
+    public ScPatternTripletController(ScPatternJsonMapper scPatternJsonMapper) {
+        this.scPatternJsonMapper = scPatternJsonMapper;
     }
 
     @GetMapping
-    public ResponseEntity<?> find(@RequestBody List<ScPatternTripletDto> triplets) {
+    public ResponseEntity<?> save(@RequestBody List<ScPatternTripletDto> triplets) {
+        ScPattern scPattern = scPatternJsonMapper.map(triplets);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
