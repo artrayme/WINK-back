348c4b00a6398b95fd75df18fa491f9ed2ea3d72
Mikhail Kraustou <m.kravczov@inbox.ru>
2022-04-18 22:04:15 +0300

    feat(engine): added rdfException

--- engine/src/main/java/org/wink/engine/converter/rdf/RdfToWinkConverter.java
+++ engine/src/main/java/org/wink/engine/converter/rdf/RdfToWinkConverter.java
@@ -1,7 +1,12 @@
 package org.wink.engine.converter.rdf;
 
+import org.wink.engine.exceptions.RdfParseException;
 import org.wink.engine.model.graph.interfaces.WinkGraph;
 
+/**
+ * @author Mikhail Krautsou
+ * @since 0.0.1
+ */
 public interface RdfToWinkConverter {
-    WinkGraph convertRdf(String rdfFileContent, String rdfFileName);
+    WinkGraph convertRdf(String rdfFileContent, String rdfFileName) throws RdfParseException;
 }
