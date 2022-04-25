93a01fe51153821e67033108e14435529838a878
artrayme <artrayme@protonmail.com>
2022-04-25 13:21:08 +0300

    feat(rdf): add controller with delete mapping

--- sc-json-http-module/src/main/java/org/wink/module/http/scg/controller/RdfController.java
+++ sc-json-http-module/src/main/java/org/wink/module/http/scg/controller/RdfController.java
@@ -4,6 +4,8 @@ import org.ostis.scmemory.model.exception.ScMemoryException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,4 +56,15 @@ public class RdfController {
             return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
         }
     }
+
+    @DeleteMapping("/delete/{name}")
+    public ResponseEntity<?> delete(@PathVariable("name") String name) {
+        try {
+            scMemoryManager.unload(name);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (ScMemoryException e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
